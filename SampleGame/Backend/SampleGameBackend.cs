eeb762fe53c33c3ebcf34f5c324474793fe7ef1e
Manny-OnGit <manny.fluss@gmail.com>
2025-03-18 16:20:41 -0700

    Revert "might redo this stuff"
    This reverts commit d18faf13f18b259a8195302f2458d71ff4c6c550.

--- SampleGame/Backend/SampleGameBackend.cs
+++ SampleGame/Backend/SampleGameBackend.cs
@@ -3,52 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 
 
-
-public class PlayerState {
-    public enum InputState{
-        ACTIVE,
-        INACTIVE,
-    };
+public struct PlayerState {
     public Vector2 position {set;get;}
     public float rotation {set;get;}
-    public InputState MyInputState {set;get;}
-    public int health {set;get;}
+
 
     public PlayerState(Vector2 _position, float _rotation){
         position = _position;
         rotation = _rotation;
-        //add options
-        health = 3;
-        MyInputState = InputState.ACTIVE;
     }
 }
-public class Projectile{
-    public Vector2 position {set;get;}
-    public Projectile(Vector2 _position){
-        position=_position;
-    }
-}
-
-//sent over the wire
 public struct SampleGameState {
+
     public Dictionary<int,PlayerState> Players {set;get;}
-    public List<Vector2> DefaultProjectilePositions {set;get;}
-    public SampleGameState(Dictionary<int,PlayerState> _Players = null, List<Vector2> _projectiles = null){
-        Players = _Players ?? new Dictionary<int,PlayerState>();
-        DefaultProjectilePositions = _projectiles ?? new List<Vector2>(); 
+
+    public SampleGameState(Dictionary<int,PlayerState> _Players = null){
+        Players = _Players ?? new Dictionary<int,PlayerState>(); 
     }
 }
 
-public class SampleGameAuthorityGameState{
-    public Dictionary<int, Projectile> Projectiles;
-    public SampleGameAuthorityGameState(){
-        Projectiles = new Dictionary<int, Projectile>();
-    }
-}
+
 
 public partial class SampleGameBackend : Node2D
 {
@@ -60,9 +37,6 @@ public partial class SampleGameBackend : Node2D
     public Dictionary<int,Character> MyPlayers = new Dictionary<int,Character>();
     private Node2D PlayersSpawnPath;
     private SampleGameState MyState = new SampleGameState();
-    private SampleGameAuthorityGameState MyAuthorityState = new SampleGameAuthorityGameState();
-    public event Action<int> PlayerHit;
-
     public override void _Ready()
     {
         base._Ready();
@@ -81,8 +55,6 @@ public partial class SampleGameBackend : Node2D
             GD.Print(id, "gotten");
             
         }
-        PlayerHit += OnPlayerHit;
-
         GD.Print(Multiplayer.GetUniqueId(), "my id");
 
 
@@ -93,10 +65,6 @@ public partial class SampleGameBackend : Node2D
             PlayersSpawnPath.AddChild(newPlayer,true);
         }
     }
-
-
-
-
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
@@ -107,8 +75,6 @@ public partial class SampleGameBackend : Node2D
         }
     }
 
-
-
     [Rpc (MultiplayerApi.RpcMode.Authority, CallLocal = false, TransferChannel = 0, TransferMode = MultiplayerPeer.TransferModeEnum.UnreliableOrdered)]
     private void replicateGameStates(string data){
         SampleGameState newState = JsonSerializer.Deserialize<SampleGameState>(data,JsonOptions);
@@ -117,37 +83,15 @@ public partial class SampleGameBackend : Node2D
 
     private void updateGameState(){
         Dictionary<int,PlayerState> toAdd = new Dictionary<int, PlayerState>(); 
-        List<Vector2> bullets = new List<Vector2>();
-        foreach (var item in MyAuthorityState.Projectiles.Values)
-        { 
-            bullets.Add(item.position);  
-        }
 
         foreach (var kvp in MyPlayers){
-            PlayerState current = new PlayerState(kvp.Value.Position, kvp.Value.Rotation);
-            
-            toAdd[kvp.Key] = current;
-            ProccessPlayer(current,kvp.Key);
+            toAdd[kvp.Key] = new PlayerState(kvp.Value.Position, kvp.Value.Rotation);
+            //GD.Print(kvp.Value.Position);
         }
-        MyState = new SampleGameState(toAdd,bullets);
+
+        MyState = new SampleGameState(toAdd);
 
         //add in some sort of rpc to send this out to all the other peers
-    }
-
-    //side effect players gets updated
-    private void OnPlayerHit(int obj)
-    {
-        PlayerState playerHit = MyState.Players[obj];
-        playerHit.health -= 1;
-    }
-
-    private void ProccessPlayer(PlayerState curr, int playerid){
-        foreach (Vector2 projectile in MyState.DefaultProjectilePositions){
-            //add setting here later
-            if (projectile.DistanceSquaredTo(curr.position) < 5 * 5){
-                PlayerHit.Invoke(playerid);
-            }
-        }
     }
 
     public void pingPlayerBall(int playerID){
@@ -169,6 +113,8 @@ public partial class SampleGameBackend : Node2D
             } else {
                 MyPlayers[playerID].Rotation = MyPlayers[playerID].Rotation;
             }
+                
+
         } else {
             GD.PushError("game state does not contain ", playerID);
         }  
@@ -177,16 +123,6 @@ public partial class SampleGameBackend : Node2D
         
     }
 
-    public int RegisterNewProjectile(Projectile _projectile){
-        MyAuthorityState.Projectiles[_projectile.GetHashCode()] = _projectile;
-        return _projectile.GetHashCode();
-    }
-
-    public void DeleteRegisteredProjectile(int id){
-        if (MyAuthorityState.Projectiles.ContainsKey(id)){
-            MyAuthorityState.Projectiles.Remove(id);
-        }
-    }
     public SampleGameState GetGameState(){
         return MyState;
     }
